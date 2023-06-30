f1022dbc534d82c8e583d7b7da2d8c7be1026638
Developsom <100343391+Developsom@users.noreply.github.com>
2023-06-30 22:21:42 +0200

    Added step

--- src/ObjectenEnKlassen/ApplePieRecipe.java
+++ src/ObjectenEnKlassen/ApplePieRecipe.java
@@ -7,5 +7,10 @@ public class ApplePieRecipe {
     public void verdeelEi(){
         System.out.println("Klop het ei los en verdeel deze in twee delen. De ene helft is voor het deeg, het andere deel is voor het bestrijken van de appeltaart.");
     }
+    public void deegBereiden(){
+        System.out.println("Meng de boter, bastard suiker, zelfrijzend bakmeel, een helft van het ei, vanille suiker en een snufje zout tot een stevig deeg en verdeel deze in 3 gelijke delen.");
+    }
     public void
+
+
 }
