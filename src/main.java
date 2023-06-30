03de17081143f1a99afbf9ae383235b2ffed299f
Developsom <100343391+Developsom@users.noreply.github.com>
2023-06-30 18:46:39 +0200

    Learning Switch statement

---
+++ src/main.java
@@ -0,0 +1,56 @@
+public class main {
+
+    private int number;
+    private int input1;
+    private int input2;
+    private String name;
+
+    public static void Main(String[]args){
+
+    }
+
+    public void hello() {
+        System.out.println("Hello, World!");
+    }
+    public void positiveOrNegativ(int number) {
+        if ( number > 0){
+            System.out.println(input1 + "is bigger than" + input2 + "!" );
+        }
+        else {
+            System.out.println("This number is negative !");
+        }
+    }
+
+    public void positiveOrZeroOrNegativ(int number) {
+        if ( number > 0) {
+            System.out.println("This number is positive !");
+        }
+        if ( number == 0) {
+            System.out.println("This number is zero !");
+        }
+        else {
+            System.out.println("This nr is neg!");
+        }
+
+    public void bartender(String name) {
+            switch (name) {
+            case 1:
+                System.out.println("Erika");
+                break;
+            case 2:
+                System.out.println("July");
+                break;
+            case 3:
+                System.out.println("Piet");
+                break;
+            case 4:
+                System.out.println("Pinas");
+                break;
+        }
+        }
+    }
+
+    public void sum(int input1, int input2) {
+
+    }
+}
