1ae287bb4c7e2a3afc08b8efdf39c6ffdf26e143
Developsom <100343391+Developsom@users.noreply.github.com>
2023-06-30 23:39:01 +0200

    //Continue from printing the methods

--- src/ObjectenEnKlassen/Main.java
+++ src/ObjectenEnKlassen/Main.java
@@ -1,8 +1,23 @@
 package ObjectenEnKlassen;
 
 public class Main {
-    private ApplePieRecipe applePieRecipe;
+    private static ApplePieRecipe applePieRecipe;
     public static void main(String[] args) {
+        applePieRecipe.appelsBereiden();
+        applePieRecipe.bakken();
+        applePieRecipe.bedekBodemEnRandMetDeeg();
+        applePieRecipe.appelsGebruik();
+        applePieRecipe.benutDeegRestant();
+        applePieRecipe.deegBereiden();
+        applePieRecipe.verdeelEi();
+        applePieRecipe.vetSpringvormIn();
+        applePieRecipe.verwarmOven();
+        applePieRecipe.deegRestant();
+    }
 
+    public static void setApplePieRecipe(ApplePieRecipe applePieRecipe) {
+        Main.applePieRecipe = applePieRecipe;
     }
 }
+
+//Continue from printing the methods
