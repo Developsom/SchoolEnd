787e14da8eef40fec6ac806ac0abeed558b2276d
Developsom <100343391+Developsom@users.noreply.github.com>
2023-06-30 22:31:28 +0200

    Finished adding steps

--- src/ObjectenEnKlassen/ApplePieRecipe.java
+++ src/ObjectenEnKlassen/ApplePieRecipe.java
@@ -10,7 +10,25 @@ public class ApplePieRecipe {
     public void deegBereiden(){
         System.out.println("Meng de boter, bastard suiker, zelfrijzend bakmeel, een helft van het ei, vanille suiker en een snufje zout tot een stevig deeg en verdeel deze in 3 gelijke delen.");
     }
-    public void
-
-
+    public void appelsBereiden(){
+        System.out.println("Schil nu de appels en snij deze in plakjes. Vermeng in een kopje de suiker en kaneel.");
+    }
+    public void vetSpringvormIn(){
+        System.out.println("Vet de springvorm in en bestrooi deze met bloem");
+    }
+    public void bedekBodemEnRandMetDeeg(){
+        System.out.println("Gebruik een deel van het deeg om de bodem van de vorm te bedekken. Gebruik een deel van het deeg om de rand van de springvorm te bekleden. Strooi het paneermeel op de bodem van de beklede vorm. De paneermeel neemt het vocht van de appels op.");
+    }
+    public void appelsGebruik(){
+        System.out.println("Doe de heft van de appels in de vorm en strooi hier 1/3 van het kaneel-suiker mengsel overheen. Meng de ander helft van de appels met het overgebleven kaneel-suiker mengsel en leg deze in de vorm.");
+    }
+    public void deegRestant(){
+        System.out.println("Rol het laatste deel van de deeg uit tot een dunne lap en snij stroken van ongeveer 1 cm breed.");
+    }
+    public void benutDeegRestant(){
+        System.out.println("Leg de stroken kuislings op de appeltaart. Met wat extra deegstroken werk je de rand rondom af. Gebruik het overgebleven ei om de bovenkant van het deeg te bestrijken");
+    }
+    public void bakken(){
+        System.out.println("Zet de taart iets onder het midden van de oven. Bak de taart in 60 minuten op 170 graden Celsius (boven en onderwarmte) gaar en goudbruin.");
+    }
 }
