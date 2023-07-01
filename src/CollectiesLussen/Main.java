843ef0b0247f36950915a31d397c15b60efeed07
Developsom <100343391+Developsom@users.noreply.github.com>
2023-07-01 21:20:18 +0200

    Working on the while loop

--- src/CollectiesLussen/Main.java
+++ src/CollectiesLussen/Main.java
@@ -1,5 +1,6 @@
 package CollectiesLussen;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -7,5 +8,13 @@ public class Main {
         String[] alphabetic = {"een", "twee", "drie", "vier", "vijf", "zes", "zeven", "acht", "negen", "nul"};
 
         Translator translator = new Translator(alphabetic, numeric);
+
+        boolean play = true;
+        String ongeldig = "Ongeldige invoer";
+        Scanner scanner = new Scanner(System.in);
+
+        while (play) {
+
+        }
     }
 }
