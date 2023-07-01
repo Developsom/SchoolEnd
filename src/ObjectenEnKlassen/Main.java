936151e45e1d9bfe760ae8630d59ebf909182168
Developsom <100343391+Developsom@users.noreply.github.com>
2023-07-01 12:57:13 +0200

    Fixed space

--- src/ObjectenEnKlassen/Main.java
+++ src/ObjectenEnKlassen/Main.java
@@ -4,7 +4,6 @@ public class Main {
     public static void main(String[] args) {
         ApplePieRecipe applePieRecipe = new ApplePieRecipe();
 
-
         applePieRecipe.printIngredients();
         applePieRecipe.printSteps();
     }
