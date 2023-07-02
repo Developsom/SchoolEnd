0aeb15beed006af3636a9a72cea92a778869088a
Developsom <100343391+Developsom@users.noreply.github.com>
2023-07-02 03:05:18 +0200

    Fixed readability

---
+++ src/PokemonPkBotBeta.java
@@ -0,0 +1,10 @@
+public class PokemonPkBotBeta {
+    public static void main(String[] args) {
+        //Een Super klasse
+        // minimaal 2 private variabelen,
+        // 1 constructor,
+        // 2 methodes,
+        // 2 getters en
+        // 2 setters;
+    }
+}
