a816fca2219c1f4a5869bd93602dbd7d1d2449fd
Developsom <100343391+Developsom@users.noreply.github.com>
2023-07-02 04:03:56 +0200

    1/1 Super, 2/4 Sub - Classes

--- src/Pokemon/Player.java
+++ src/Pokemon/Player.java
@@ -1,18 +1,41 @@
 package Pokemon;
 
 public class Player extends PkBot {
+    private int strength;
+    private int xpCounter;
 
-    public Player(int hp, int pray) {
+
+    public Player(int hp, int pray, int strength, int xpCounter) {
         super(hp, pray);
+        this.strength = strength;
+        this.xpCounter = xpCounter;
     }
+
 
     @Override
-    public void chechHp() {
-
-    }
+    public void chechHp() {}
 
     @Override
-    public void checkPray() {
+    public void checkPray() {}
 
+    public void checkStrength(){};
+
+    public void checkXpCounter(){}
+
+
+    public int getStrength() {
+        return strength;
     }
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public int getXpCounter() {
+        return xpCounter;
+    }
+    public void setXpCounter(int xpCounter) {
+        this.xpCounter = xpCounter;
+    }
+
+
 }
---
+++ src/Pokemon/SitDown.java
@@ -0,0 +1,48 @@
+package Pokemon;
+
+public class SitDown extends PkBot {
+    boolean KoPotential; //Checkhp if it is within maxhit
+    int MaxHit;
+
+
+    public SitDown(int hp, int pray, boolean koPotential, int maxHit) {
+        super(hp, pray);
+        KoPotential = koPotential;
+        MaxHit = maxHit;
+    }
+
+
+    @Override
+    public void chechHp() {
+
+    }
+
+    @Override
+    public void checkPray() {
+
+    }
+
+    public void checkKoPotential(){
+
+    }
+    public void checkMaxhit(){
+
+    }
+
+    public boolean isKoPotential() {
+        return KoPotential;
+    }
+
+    public void setKoPotential(boolean koPotential) {
+        KoPotential = koPotential;
+    }
+
+    public int getMaxHit() {
+        return MaxHit;
+    }
+
+    public void setMaxHit(int maxHit) {
+        MaxHit = maxHit;
+    }
+
+}
