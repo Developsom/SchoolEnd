7aa349658f0b72aa79aec917ae4b2c6a18761b13
Developsom <100343391+Developsom@users.noreply.github.com>
2023-07-05 22:54:37 +0200

    Lazy

---
+++ src/FamilyTree/Person.java
@@ -0,0 +1,14 @@
+package FamilyTree;
+
+public class Person{
+    name
+            middleName
+    lastName
+            sex
+    age
+    mother (Person)
+    father (Person)
+    siblings (lijst)
+    children (lijst)
+    pets (lijst)
+        }
