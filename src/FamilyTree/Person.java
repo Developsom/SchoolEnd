6f7270f85dff86df70ee96ebffa99f09df916a85
Developsom <100343391+Developsom@users.noreply.github.com>
2023-07-06 22:05:37 +0200

    //Took a while to remember how to instance a List

--- src/FamilyTree/Person.java
+++ src/FamilyTree/Person.java
@@ -5,19 +5,14 @@ public class Person {
         String name;
         String middleName;
         String lastName;
-
         String sex;
         String age;
         String mother;
         String father;
 
         List<String> siblings;
-
         List<String> children;
-
         List<String> pets; //Took a while to remember how to instance a List
-
-
 }
 //Start with required classes
 //Finished required classes
