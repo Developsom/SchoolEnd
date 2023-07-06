1f44db274b5049f1ac462665fe2b31e18c407dd8
Developsom <100343391+Developsom@users.noreply.github.com>
2023-07-06 20:47:38 +0200

    Removed lazy code

--- src/FamilyTree/Person.java
+++ src/FamilyTree/Person.java
@@ -3,14 +3,4 @@ package FamilyTree;
 import java.util.List;
 
 public class Person{
-    String name
-    String middleName
-    String lastName
-    String sex
-    int age
-    String mother (Person)
-    String father (Person)
-    List siblings (lijst)
-    List (lijst)
-    List pets (lijst)
         }
