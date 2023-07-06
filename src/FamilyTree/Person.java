290b3e81f869de1efbdf7935272bb9364d3c2336
Developsom <100343391+Developsom@users.noreply.github.com>
2023-07-06 21:19:04 +0200

    //Took a while to remember how to instance a List

--- src/FamilyTree/Person.java
+++ src/FamilyTree/Person.java
@@ -1,9 +1,24 @@
 package FamilyTree;
-
 import java.util.List;
 
-public class Person{
-        }
+public class Person {
+        String name;
+        String middleName;
+        String lastName;
+
+        String sex;
+        String age;
+        String mother;
+        String father;
+
+        List<String> siblings;
+
+        List<String> children;
+
+        List<String> pets; //Took a while to remember how to instance a List
+
+
+}
 //Start with required classes
 //Finished required classes
 
